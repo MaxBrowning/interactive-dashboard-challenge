93160fa8e94dec6dfcb1bff91eecd6512b0340e4
MaxBrowning <browning.max@gmail.com>
2020-12-28 19:58:31 -0500

    app in progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,61 @@
-// Fetch the JSON data and console log it
-d3.json('../../samples.json').then(function(data) {
-    console.log(data);
-  });
-
-// Promise Pending
-const dataPromise = d3.json('../../samples.json');
-console.log("Data Promise: ", dataPromise);
-
-// Initializes the page with a default plot
-function init() {
-    var trace1 = {
-        x: data.map(row => row.pair),
-        y: data.map(row => row.greekSearchResults),
-        text: data.map(row => row.greekName),
-        name: "Greek",
-        type: "bar"
-    };
+// Submit Button handler
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
   
-    Plotly.newPlot('hbar', trace1);
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var person = dropdownMenu.node().value;
+
+    // Build the plot with the new person
+    buildPlot(person);
   }
+
+// function unpack(rows, index) {
+//     return rows.map(function(row) {
+//       return row[index];
+//     });
+//   };
+
+
+// function buildPlot(person) {
+//   d3.json('../../samples.json').then(function(data) {
+
+//     // Grab values from the data json object
+//     var id = unpack(data.names);
+//     var ethnicity = data.metadata.ethnicity;
+//     var gender = data.metadata.gender;
+//     var age = data.metadata.age;
+//     var location = data.metadata.location;
+//     var bbtype = data.metadata.bbtype;
+//     var wfreq = data.metadata.wfreq;
+//     var otu_ids = unpack(data.samples.otu_ids, 0)
+//     var sample_values = unpack(data.samples.sample_values, 0)
+ 
+//     // var trace1 = {
+//     //   type: "bar",
+//     //   x: sample_values,
+//     //   y: otu_ids,
+//     //   orientation: 'h'
+//     // };
+  
+//     // var data = [trace1];
+
+//     // var layout = {
+//     //   title: `Top Ten Bacteria`,
+//     //   xaxis: {
+//     //     autorange: true
+//     //   },
+//     //   yaxis: {
+//     //     autorange: true,
+//     //     type: "linear"
+//     //   }
+//     // };
+  
+//     // Plotly.newPlot("#bar", data, layout);
+  
+//   });
+// }
+  
+// buildPlot();
